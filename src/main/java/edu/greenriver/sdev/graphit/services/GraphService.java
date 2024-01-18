425336da2dde03c104d8ea1b11f2a014e5cd3d41
David Kurilla <Kurilla.David@student.greenriver.edu>
2024-01-18 10:24:37 -0800

    DEV: Refactored and remodeled course and graph models.

---
+++ src/main/java/edu/greenriver/sdev/graphit/services/GraphService.java
@@ -0,0 +1,49 @@
+package edu.greenriver.sdev.graphit.services;
+
+import edu.greenriver.sdev.graphit.models.Course;
+import edu.greenriver.sdev.graphit.models.CourseDigraph;
+
+import java.util.List;
+
+// CLASS: GraphService
+public class GraphService {
+
+    // FIELDS
+    private CourseDigraph graph;
+
+    // CONSTRUCTOR
+    public GraphService() {
+        graph = new CourseDigraph();
+    }
+
+    // METHOD: addCourse
+    public void addCourse(Course course) {
+        graph.addVertex(course);
+    }
+
+    // METHOD: addPrereq
+    public boolean addPrereq(Course source, Course destination) {
+        graph.addEdge(source, destination);
+        if (graph.hasCycle()) {
+            graph.removeEdge(source, destination);
+            return false;
+        }
+        return true;
+    }
+
+    // METHOD: removeCourse
+    public void removeCourse(Course course) {
+        graph.removeVertex(course);
+    }
+
+    // METHOD: removePrereq
+    public void removePrereq(Course source, Course destination) {
+        graph.removeEdge(source, destination);
+    }
+
+    // METHOD: sortCourses
+    public List<Course> sortCourses() {
+        return graph.sortCourses();
+    }
+
+}
